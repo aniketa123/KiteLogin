92753eb8a47ab23c070dfc2a7bd8f985dfe3353a
aniketa123 <106725198+aniketa123@users.noreply.github.com>
2022-06-02 12:00:24 +0530

    Add files via upload

---
+++ ConcatString.java
@@ -0,0 +1,30 @@
+package Demo;
+
+public class ConcatString 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		String str="Aniketa";
+		String str1="Mahesh";
+		String str2="Panaskar";
+		
+		System.out.println(str.concat(str1).concat(str2));
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
